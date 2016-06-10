1f597dfa9219f561cc9cab2a561efc3bbbde9bb7
Chris Loew <cloew123@gmail.com>
2016-06-09 20:09:15 -0600

    Added basic fields to the setup script

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(name='kao_hex',
       version='0.0.1',
       description="",
-      author='',
-      author_email='',
+      author='Chris Loew',
+      author_email='cloew123@gmail.com',
       packages=['kao_hex'],
      )
