4b17ac54d4488406b7fd0c55aea7fe90440a55fc
Chris Loew <cloew123@gmail.com>
2016-06-09 20:15:24 -0600

    Added property to retrieve the neighbors of a grid coordinate

--- kao_hex/grid_coord.py
+++ kao_hex/grid_coord.py
@@ -12,6 +12,12 @@ class GridCoord:
         """ Return the z coordinate """
         return -self.x - self.y
         
+    @property
+    def neighbors(self):
+        """ Return the neighboring coordinates """
+        return [GridCoord(x=self.x+1, y=self.y), GridCoord(x=self.x, y=self.y+1), GridCoord(x=self.x-1, y=self.y+1),
+                GridCoord(x=self.x-1, y=self.y), GridCoord(x=self.x, y=self.y-1), GridCoord(x=self.x+1, y=self.y-1)]
+        
     def __repr__(self):
         """ Return the String representation of the Grid Coord """
         return "<GridCoord(x={}, y={}, z={})>".format(self.x, self.y, self.z)
